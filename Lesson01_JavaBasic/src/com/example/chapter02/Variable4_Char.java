31e06121c348118e846f9e6d55552d7d5bc8dea7
ish1213 <89083100+ish1213@users.noreply.github.com>
2021-08-23 21:53:18 +0900

    2021-08-23

---
+++ Lesson01_JavaBasic/src/com/example/chapter02/Variable4_Char.java
@@ -0,0 +1,33 @@
+package com.example.chapter02;
+
+public class Variable4_Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char myChar = 'A';     //작은따옴표로 묶인 것은 문자
+		String yourStr = "Apple"; //큰따옴표로 묶인 것은 문자열...
+		
+		System.out.println(myChar);// char A를 출력
+		System.out.println(yourStr);// string A를 출력
+		
+		//char 타입의 변수는 ascii 로 출력할 수 있다.
+		System.out.println((int)myChar);  // 65
+		
+		int newChar = 66;
+		//ascii 코드값울 뮨자로 출력할 수 있다.
+		System.out.println((char)newChar); // B
+		
+		// 영어알파벳 A ~ Z까지를 아스키코드를 이용 출력
+		for(int ch = 65; ch < 91;ch++) {
+			System.out.println((char)ch);
+		}
+		
+		char ch1 = '한';
+	    char ch2 = '\uD55C';
+	    
+	    System.out.println(ch1);
+	    System.out.println(ch2);
+
+	}
+
+}
---
+++ Lesson01_JavaBasic/src/com/example/chapter03/Operator1_Increase.java
@@ -0,0 +1,22 @@
+package com.example.chapter03;
+
+public class Operator1_Increase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b;
+		//a= a + 1;
+		//a++;
+		b = ++a;
+		// a = a - 1;
+		// a--; // --a;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
+
+	}
+
+}
